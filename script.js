211f55e35d06210555ed500681283892a44b5a8b
Paulo Henrique <paulo.henrique.19@hotmail.com>
2021-08-27 10:44:12 -0300

    Primeiro codigo
    Programa funcionando, mas ainda esta grande.

---
+++ script.js
@@ -0,0 +1,11 @@
+function clicar1() {
+    window.alert('Você clicou no 1º Botão!');
+}
+
+function clicar2() {
+    window.alert('Você clicou no 2º Botão!');
+}
+
+function clicar3() {
+    window.alert('Você clicou no 3º Botão!');
+}

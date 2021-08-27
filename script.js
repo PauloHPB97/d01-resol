ec1a3a1ab4d1d5fa9b8025afec05518c3664056a
Paulo Henrique <paulo.henrique.19@hotmail.com>
2021-08-27 10:50:58 -0300

    Aprimoramento do codigo
    Aprimorando o codigo para ficar menor

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function clicar1() {
-    window.alert('Você clicou no 1º Botão!');
-}
-
-function clicar2() {
-    window.alert('Você clicou no 2º Botão!');
-}
-
-function clicar3() {
-    window.alert('Você clicou no 3º Botão!');
+function clicar(num) {
+    if (num === 1) {
+        window.alert('Você clicou no 1º Botão!')
+    } else if (num === 2) {
+        window.alert('Você clicou no 2º Botão!')
+    } else if (num === 3) {
+        window.alert('Você clicou no 3º Botão!')
+    } else {
+        window.alert('[ERRO] Houve algo erro desconhecido! :(')
+    }
 }
